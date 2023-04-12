9eeae5a62a9c9975e1b27d3ef6fa7e52e8257270
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-04-12 05:34:04 -0400

    ci: update the publish script :green_heart:

--- pusblish.py
+++ pusblish.py
@@ -1,6 +1,32 @@
 import os
 
-version = "0.1.2"
+import subprocess
+
+last_tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode().strip()
+
+commit_message = subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode().strip()
+
+if 'feat' in commit_message:
+    version_type = 'minor'
+elif 'fix' in commit_message:
+    version_type = 'patch'
+else:
+    version_type = 'other'
+
+if version_type == 'other':
+    new_version = last_tag
+else:
+    major, minor, patch = [int(x) for x in last_tag.split('.')]
+    if version_type == 'patch':
+        patch += 1
+    elif version_type == 'minor':
+        minor += 1
+        patch = 0
+    new_version = f'{major}.{minor}.{patch}'
+
+version = new_version
+print(f'New version: {version}')
+exit()
 os.system(f"sed -i 's/version=\".*\"/version=\"{version}\"/' setup.py")
 
 os.system("rm -rf dist")

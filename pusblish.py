8b843538e63e39501a1e4b78d638c9d342224703
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-04-12 00:11:52 -0400

    feat: add script to upload to pypi.org and update the versioning of publication :sparkles:

---
+++ pusblish.py
@@ -0,0 +1,14 @@
+import os
+
+version = "0.1.1"
+os.system(f"sed -i 's/version=\".*\"/version=\"{version}\"/' setup.py")
+
+os.system("rm -rf dist")
+os.system("rm -rf build")
+os.system("rm -rf localizedpydantic.egg-info")
+
+os.system("python setup.py sdist bdist_wheel")
+
+os.system("twine check dist/*")
+
+os.system("twine upload dist/*")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="localizedpydantic",
-    version="0.1.0",
+    version="0.1.1",
     author="Lucas Balieiro Matos",
     author_email="lucasbalieirom@hotmail.com",
     description="Library to validate localized data with pydantic",

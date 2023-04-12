425c6576c18854cd6bb08cf7ff5a7c82fa0b7506
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-04-12 05:04:06 -0400

    feat: add cnpj validation :sparkles:

--- localizedpydantic/models/brazil.py
+++ localizedpydantic/models/brazil.py
@@ -1,11 +1,22 @@
+import re
 from pydantic import BaseModel, validator
-from localizedpydantic.validators.brazil import validate_cpf
+from localizedpydantic.validators.brazil import validate_cpf, validate_cnpj
 class CPF(BaseModel):
     cpf: str
 
     @validator('cpf')
     def validate_cpf(cls, v):
+        v = re.sub('[^0-9]', '', v) 
         if not validate_cpf(v):
             raise ValueError('Invalid CPF')
         return f'{v[:3]}.{v[3:6]}.{v[6:9]}-{v[9:]}'
 
+class CNPJ(BaseModel):
+    cnpj: str
+
+    @validator('cnpj')
+    def validate_cnpj(cls, v):
+        v = re.sub(r'\D', '', v)
+        if not validate_cnpj(v):
+            raise ValueError('Invalid CNPJ')
+        return f'{v[:2]}.{v[2:5]}.{v[5:8]}/{v[8:12]}-{v[12:]}'
--- localizedpydantic/validators/brazil.py
+++ localizedpydantic/validators/brazil.py
@@ -1,8 +1,6 @@
 import re
 
 def validate_cpf(cpf: str) -> bool:
-    cpf = re.sub('[^0-9]', '', cpf)
-
     if len(cpf) != 11:
         return False
 
@@ -24,3 +22,32 @@ def validate_cpf(cpf: str) -> bool:
         return False
 
     return True
+
+def validate_cnpj(cnpj: str) -> bool:
+    cnpj = re.sub(r'\D', '', cnpj)
+
+    if not cnpj.isdigit():
+        return False
+
+    if len(cnpj) != 14:
+        return False
+
+    invalid_numbers = [str(i) * 14 for i in range(10)]
+    if cnpj in invalid_numbers:
+        return False
+
+    weight1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    weight2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+    cnpj_digit = cnpj[-2:]
+    cnpj = cnpj[:-2]
+
+    total1 = sum([int(a) * b for a, b in zip(cnpj, weight1)])
+    remainder1 = total1 % 11
+    digit1 = str(11 - remainder1) if remainder1 > 1 else '0'
+
+    total2 = sum([int(a) * b for a, b in zip(cnpj + digit1, weight2)])
+    remainder2 = total2 % 11
+    digit2 = str(11 - remainder2) if remainder2 > 1 else '0'
+
+    return cnpj_digit == digit1 + digit2
